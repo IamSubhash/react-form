daf268686a5c5b486f34e11e62c0cc749f792f3e
IamSubhash <subhash.52525@gmail.com>
2024-09-20 13:25:42 +0530

    Redux flow

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import React, {useState} from 'react';
 import './style.css';
+import {useDispatch, useSelector} from 'react-redux';
+import { loginUser } from './redux/actions';
 
 export default function App() {
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+  const [credentials, setCredentials] = useState({username: '', password: ''})
   const [errorMsg, setErrorMsg] = useState('');
+
+  const dispatch = useDispatch();
+  const sessionId = useSelector((state) => state.sessionId);
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if ((username == "" | undefined) || (password == "" | undefined))
+    if ((credentials.username == "" | undefined) || (credentials.password == "" | undefined))
       setErrorMsg("Username and Password are required")
     else {
       setErrorMsg('');
-      alert("form Submitted");
+      dispatch(loginUser(credentials));
     }
   };
   return (
@@ -24,11 +28,11 @@ export default function App() {
       <form onSubmit={handleSubmit}>
         <div>
           <span>Username</span>
-          <input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+          <input type="text" value={credentials.username} onChange={(e) => setCredentials({...credentials, username: e.target.value})} />
         </div>
         <div>
           <span>Password</span>
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+          <input type="password" value={credentials.password} onChange={(e) => setCredentials({...credentials, password: e.target.value})} />
         </div>
         <p style={{color : 'red'}}>{errorMsg}</p>
         <div>
